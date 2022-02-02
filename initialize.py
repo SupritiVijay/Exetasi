0f9bdbcc6fad42ca14141c08709e876ff5d3536a
SupritiVijay <59250204+SupritiVijay@users.noreply.github.com>
2022-02-02 23:04:42 +0530

    Add files via upload
    Initializing all data files

--- initialize.py
+++ initialize.py
@@ -1,4 +1,4 @@
-import numpy ad np
+import numpy as np
 import pandas as pd
 
 words = pd.read_csv('GREWordList.csv')
@@ -6,16 +6,13 @@ n = len(words.Word)
 unused_words = [0]*n
 fre_mistaken_words = [0]*n
 l1 = [0]*n
-word_confusion = []
+word_confusion = [l1]*n
 
-for i in n:
-	word_confusion = word_confusion.append(l1)
+uw = pd.DataFrame(unused_words, columns=["Unused_Words_Frequency"])
+uw.to_csv("./data/unused_words.csv",index = False)
 
-uw = pd.Dataframe(unused_words, ["Unused_Words_Frequency"])
-uw.to_csv("unused_words.csv",index = False)
+fre_mw = pd.DataFrame(fre_mistaken_words, columns=["Frequently Mistaken Words Frequency"])
+fre_mw.to_csv("./data/frequently_mistaken_words.csv", index = False)
 
-fre_mw = pd.Dataframe(fre_mistaken_words, ["Frequently Mistaken Words Frequency"])
-fre_mw.to_csv("frequently_mistaken_words.csv", index = False)
-
-word_con = pd.Dataframe(word_confusion, words.Word)
-word_con.to_csv("word_confusion_matrix.csv", index = False)
+word_con = pd.DataFrame(word_confusion, columns=words.Word, index=words.Word)
+word_con.to_csv("./data/word_confusion_matrix.csv")
